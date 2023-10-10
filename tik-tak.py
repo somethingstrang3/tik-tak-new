2574db82db60d02b41d20c3213e7ab884b30c47a
somethingstrang3 <stk15367@gmail.com>
2023-10-10 15:02:08 +0300

    Update tik-tak.py

--- tik-tak.py
+++ tik-tak.py
@@ -5,18 +5,44 @@ pobeda = [(1,2,3),(4,5,6),(7,8,9),(3,6,9),(2,5,8),(1,4,7),(1,5,9),(3,5,7)] #Вы
 def f():
     for i in range(3):
         print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3],'|')
-        
-def main():
+
+def who(X_or_O):
     while True:
-        f()
-        if count%2 == 0:
-            who("X") #Это фунция которую я вызову позже
-        else:
-            who("O")
-            
+        value = input('Выберите место для постановки'+X_or_O)
+        if value not in '123456789':
+            print('Ошибка.Повторите ввод.')
+            continue
+        value = int(value)
+        if str(board[value-1]) in 'XO': #так как индексы начинаются с 0,мы наше число (от 1 до 9) уменьшаем на один.
+            print('Вы выбрали занятуюю ячейку')
+            continue
+        board[value-1] = X_or_O
+        break
+
 def check_Winner():
     for j in pobeda:
         if (board[j[0]-1]) == (board[j[1]-1]) == (board[j[2]-1]):
             return (board[j[1]-1])
     else:
         return False
+    
+def main():
+    count = 0
+    while True:
+        f()
+        if count%2 == 0:
+            who("X") #Это фунция которую я вызову позже
+        else:
+            who("O")
+        if count > 3: #Как только больше 3 ходов прошло ,начинаем проверять на победу
+            winner = check_Winner()
+            if winner:
+                f()
+                print(winner,"Победил")
+                break
+        count +=1
+        if count >=9: #Проверка ничью
+            f()
+            print('Ничья')
+            break
+main()
