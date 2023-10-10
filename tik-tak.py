9b7ccb04f0f12b5b40ff9c7c8d234a0a6bbb6b0c
somethingstrang3 <stk15367@gmail.com>
2023-10-10 14:54:49 +0300

    Update tik-tak.py

--- tik-tak.py
+++ tik-tak.py
@@ -1,8 +1,22 @@
 board = list(range(1, 10))
 
+pobeda = [(1,2,3),(4,5,6),(7,8,9),(3,6,9),(2,5,8),(1,4,7),(1,5,9),(3,5,7)] #Выигрышные комбинации
+
 def f():
     for i in range(3):
         print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3],'|')
-print('Выбор ячейки')
-a = input()
-print('Выбрали ',a ' ячейку')
+        
+def main():
+    while True:
+        f()
+        if count%2 == 0:
+            who("X") #Это фунция которую я вызову позже
+        else:
+            who("O")
+
+def check_Winner():
+    for j in pobeda:
+        if (board[j[0]-1]) == (board[j[1]-1]) == (board[j[2]-1]):
+            return (board[j[1]-1])
+    else:
+        return False
